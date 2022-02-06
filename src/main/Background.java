430a924cfed27cad15be85b9738458084a532acc
CheeseyBob <cheeseybobdev@gmail.com>
2022-02-06 13:21:45 +0000

    Finished Background Paths for Outcasts.

--- src/main/Background.java
+++ src/main/Background.java
@@ -13,27 +13,14 @@ public class Background {
 		this.aptitude = aptitude;
 	}
 	
-	public Background withStartingPaths(Path path) {
+	public Background withStartingPath(Path path) {
 		backgroundPathList.add(path);
-		return this;
-	}
-	
-	public Background withStartingPaths(Path path1, Path path2) {
-		backgroundPathList.add(path1);
-		backgroundPathList.add(path2);
-		return this;
-	}
-	
-	public Background withStartingPaths(Path path1, Path path2, Path path3) {
-		backgroundPathList.add(path1);
-		backgroundPathList.add(path2);
-		backgroundPathList.add(path3);
 		return this;
 	}
 	
 	public static final Background[] list = {
 			new Background("Adeptus Administratum", "Knowledge or Social")
-			.withStartingPaths(
+			.withStartingPath(
 					new Path("Scholar",
 							new String[] {
 									"Intelligence +10"
@@ -57,7 +44,9 @@ public class Background {
 									Tool.dataTome_lore,
 									MiscItem.chrono
 							}
-					),
+					)
+			)
+			.withStartingPath(
 					new Path("Operator",
 							new String[] {
 									"Agility +5",
@@ -81,7 +70,9 @@ public class Background {
 									Clothing.administratumRobes,
 									MiscItem.chrono
 							}
-					),
+					)
+			)
+			.withStartingPath(
 					new Path("Chirurgeon",
 							new String[] {
 									"Intelligence +15",
@@ -108,10 +99,10 @@ public class Background {
 									Consumable.injector_deTox,
 									MiscItem.chrono
 							}
-							)
-					),
+					)
+			),
 			new Background("Adeptus Arbites", "Offence or Defence")
-			.withStartingPaths(
+			.withStartingPath(
 					new Path("Arbitrator",
 							new String[] {
 									"Weapon Skill +10",
@@ -150,10 +141,10 @@ public class Background {
 									MiscItem.handVox,
 									MiscItem.lhoSticks
 							}
-							)
-					),
+					)
+			),
 			new Background("Adeptus Astra Telepathica", "Defense or Psyker")
-			.withStartingPaths(
+			.withStartingPath(
 					new Path("xxxxxxxxxx",
 							new String[] {
 									
@@ -168,9 +159,9 @@ public class Background {
 									
 							}
 					)
-					),
+			),
 			new Background("Adeptus Mechanicus", "Knowledge or Tech")
-			.withStartingPaths(
+			.withStartingPath(
 					new Path("Tech-Priest",
 							new String[] {
 									"Toughness +5",
@@ -206,7 +197,9 @@ public class Background {
 									MiscItem.glowGlobe,
 									Tool.lascutter,
 							}
-					),
+					)
+			)
+			.withStartingPath(
 					new Path("Tech-Chirurgeon",
 							new String[] {
 									"Intelligence +10",
@@ -241,7 +234,9 @@ public class Background {
 									Tool.surgicalTools,
 									MiscItem.glowGlobe,
 							}
-							),
+					)
+			)
+			.withStartingPath(
 					new Path("Tech-Militant",
 							new String[] {
 									"Ballistic Skill +15",
@@ -278,10 +273,10 @@ public class Background {
 									Tool.auspex,
 									MiscItem.lampPack,
 							}
-							)
-					),
+					)
+			),
 			new Background("Adeptus Ministorum", "Leadership or Social")
-			.withStartingPaths(
+			.withStartingPath(
 					new Path("Cleric",
 							new String[] {
 									"Weapon Skill +5",
@@ -319,10 +314,10 @@ public class Background {
 									Tool.tome_imperialCreed,
 									new Item("Aquilla Necklace", 0),
 							}
-							)
-					),
+					)
+			),
 			new Background("Imperial Guard", "Fieldcraft or Leadership")
-			.withStartingPaths(
+			.withStartingPath(
 					new Path("Veteran",
 							new String[] {
 									"Weapon Skill +10",
@@ -354,25 +349,113 @@ public class Background {
 									MeleeWeapon.knife,
 									MiscItem.lhoSticks,
 							}
-							)
-					),
+					)
+			),
 			new Background("Outcast", "Fieldcraft or Social")
-			.withStartingPaths(
-					new Path("xxxxxxxxxx",
+			.withStartingPath(
+					new Path("Rogue",
 							new String[] {
-									
+									"Agility +10",
+									"Intelligence +5",
+									"Perception +5",
+									"Fellowship +10",
 							},
 							new Skill[] {
-									
+									Skill.charm.withBonus(10),
+									Skill.deceive.withBonus(10),
+									Skill.run.withBonus(10),
+									Skill.search.withBonus(10),
+									Skill.sneak.withBonus(20),
+									Skill.unlock.withBonus(20),
 							},
 							new SpecialRule[] {
-									
+									Talent.accustomedToDarkness,
+									Talent.knowledgable.withParameter("Underworld"),
+									Talent.language.withParameter("Low Gothic"),
+									Talent.quickEntry,
+									Talent.sprinter,
+									Talent.trade_survivalist,
+									Talent.trade_swindler,
+									Talent.tricksOfTheTrade,
 							},
 							new Item[] {
-									
+									Clothing.streetClothes,
+									Tool.multiKey,
+									Tool.stummer,
+									RangedWeapon.pistol,
+									MeleeWeapon.knife,
+									MiscItem.gelt,
 							}
-							)
 					)
+			)
+			.withStartingPath(
+					new Path("Ganger",
+							new String[] {
+									"Weapon Skill +10",
+									"Ballistic Skill +10",
+									"Strength +10",
+									"Toughness +10",
+									"Agility +5",
+									"Perception +5",
+							},
+							new Skill[] {
+									Skill.dodge.withBonus(10),
+									Skill.intimidate.withBonus(20),
+									Skill.notice.withBonus(10),
+									Skill.reflex.withBonus(10),
+									Skill.run.withBonus(10),
+									Skill.search.withBonus(10),
+									Skill.sneak.withBonus(10),
+									Skill.unlock.withBonus(0),
+							},
+							new SpecialRule[] {
+									Talent.knowledgable.withParameter("Underworld"),
+									Talent.language.withParameter("Low Gothic"),
+							},
+							new Item[] {
+									Armour.gangLeathers,
+									Clothing.streetClothes,
+									RangedWeapon.autopistol,
+									Ammo.autopistol_mag,
+									MeleeWeapon.club,
+									MeleeWeapon.knife,
+									MiscItem.gelt,
+							}
+					)
+			)
+			.withStartingPath(
+					new Path("Assassin",
+							new String[] {
+									"Weapon Skill +10",
+									"Ballistic Skill +10",
+									"Agility +5",
+									"Perception +5",
+							},
+							new Skill[] {
+									Skill.conceal.withBonus(20),
+									Skill.deceive.withBonus(20),
+									Skill.disguise.withBonus(20),
+									Skill.sneak.withBonus(20),
+							},
+							new SpecialRule[] {
+									Talent.accustomedToDarkness,
+									Talent.assassinStrike,
+									Talent.carefulStrikes,
+									Talent.knowledgable.withParameter("Underworld"),
+									Talent.language.withParameter("Low Gothic"),
+									Talent.marksman,
+							},
+							new Item[] {
+									Clothing.streetClothes,
+									RangedWeapon.huntingRifle_scoped,
+									RangedWeapon.pistol,
+									Ammo.huntingRifle_mag,
+									MeleeWeapon.longDagger,
+									Consumable.vial_lethalPoison_3,
+									MiscItem.gelt,
+							}
+					)
+			)
 	};
 	public static final String[][] roleRollList = {
 			{"01-02", "03", "04-28", "29-48", "49", "50-79", "80-99", "00"},
